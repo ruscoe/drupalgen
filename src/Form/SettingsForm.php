9b76b6ffad0fce3ead02c72ae85db7afb8147ae6
ruscoe <danruscoe@protonmail.com>
2025-04-17 17:52:54 -0700

    Add model and token settings to configuration form.

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -7,6 +7,9 @@ use Drupal\Core\Form\FormStateInterface;
 
 class SettingsForm extends ConfigFormBase {
 
+  const DEFAULT_COMPLETION_TOKENS = 128;
+  const DEFAULT_MODEL = 'gpt-4o';
+
   /**
    * {@inheritdoc}
    */
@@ -27,11 +30,32 @@ class SettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('drupalgen.settings');
 
+    // Add a text field for the OpenAI API key.
     $form['openai_api_key'] = [
       '#type' => 'textfield',
       '#title' => $this->t('OpenAI API Key'),
       '#default_value' => $config->get('openai_api_key'),
       '#description' => $this->t('Enter your OpenAI API key.'),
+    ];
+
+    // Add a text field for the max completion tokens.
+    $form['max_completion_tokens'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Max Completion Tokens'),
+      '#default_value' => $config->get('max_completion_tokens') ? $config->get('max_completion_tokens') : self::DEFAULT_COMPLETION_TOKENS,
+      '#description' => $this->t('Enter the maximum number of tokens to use per API response.'),
+    ];
+
+    // Add a select field for the model.
+    $form['model'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Model'),
+      '#options' => [
+        'gpt-4o' => $this->t('GPT-4 OpenAI'),
+        'gpt-3.5-turbo' => $this->t('GPT-3.5 Turbo'),
+      ],
+      '#default_value' => $config->get('model') ? $config->get('model') : self::DEFAULT_MODEL,
+      '#description' => $this->t('Select the ChatGPT model to use.'),
     ];
 
     return parent::buildForm($form, $form_state);
@@ -43,6 +67,8 @@ class SettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('drupalgen.settings')
       ->set('openai_api_key', $form_state->getValue('openai_api_key'))
+      ->set('max_completion_tokens', $form_state->getValue('max_completion_tokens'))
+      ->set('model', $form_state->getValue('model'))
       ->save();
 
     parent::submitForm($form, $form_state);
